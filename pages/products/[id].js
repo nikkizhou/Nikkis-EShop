25cf412ba27595c1ec29365fff1261ecde56095e
unknown <nikkizhou99@gmail.com>
2022-10-25 17:25:23 +0200

    done with SSR

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,13 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Image from 'next/image';
-import { useRouter } from 'next/router'
-import products from '../../data/data.json'
 import styles from '../../styles/Product.module.css'
 
-function Product() {
-  const router = useRouter()
-  const {id} = router.query
-  const product = products.find(pro => pro.id == id)
+function Product({product}) {
+  // const router = useRouter()
+  // const {id} = router.query
+  // const product = products.find(pro => pro.id == id)
   
   return (
     <div className={styles.product}>
@@ -22,6 +20,21 @@ function Product() {
       <p>{product.description }</p>
     </div>
   )
+}
+
+//generate generate dynamic routing page statically with getStaticPaths() and getStaticProps()
+export async function getStaticPaths() {
+  const res = await fetch('https://fakestoreapi.com/products')
+  const products = await res.json()
+  const paths = products.map((pro) => ({ params: { id: pro.id.toString() }}))
+  return { paths, fallback: false }
+}
+
+export async function getStaticProps({ params }) {
+  // the params comes from getStaticPath 
+  const res = await fetch(`https://fakestoreapi.com/products/${params.id}`)
+  const product = await res.json()
+  return { props: { product } }
 }
 
 export default Product
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-import products from '../../data/data.json'
 import Link from 'next/link'
 import Image from 'next/image';
 import styles from '../../styles/ProductList.module.css'
 import CategoryFilter from '../../components/CategoryFilter'
-function ProductList() {
+
+function ProductList({products}) {
   const [selectedCategory, setSelectedCategory] = useState();
   const updateCategory = (cate) => setSelectedCategory(cate);
   const productsDisplay = selectedCategory ? products.filter(pro=>pro.category==selectedCategory) : products
@@ -29,9 +29,21 @@ function ProductList() {
       </div>
 
     </div>
-
-
   )
+}
+
+`The context object has:
+1.Params - /pages/customer/[id].js/ =>
+    context.params.id
+2. Request and Response - context.req / context.res
+3. Querystring - index.js?archived=true =>
+    context.query.archived`
+export async function getServerSideProps(context) {
+  const res = await fetch('https://fakestoreapi.com/products')
+  const products = await res.json()
+
+  if (!products) return { redirect: { destination: '/', permanent: false, } }
+  return { props: { products } }
 }
 
 export default ProductList
